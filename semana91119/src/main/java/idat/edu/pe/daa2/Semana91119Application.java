7322cf270b60d3011a7fc78af672fc936ff27230
Administrador <Administrador@IDATCOMPUTACION>
2019-11-20 22:43:17 -0500

    ERT

---
+++ semana91119/src/main/java/idat/edu/pe/daa2/Semana91119Application.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.daa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semana91119Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Semana91119Application.class, args);
+	}
+
+}
---
+++ semana91119/src/test/java/idat/edu/pe/daa2/Semana91119ApplicationTests.java
@@ -0,0 +1,12 @@
+package idat.edu.pe.daa2;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana91119ApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
